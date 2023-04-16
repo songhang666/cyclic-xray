0562ff5ba3c2dc5d8730e4eb84dcf30e83c1a5ae
songhang <751294847@qq.com>
2023-04-16 16:06:22 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const username = process.env.WEB_USERNAME || "admin";
-const password = process.env.WEB_PASSWORD || "password";
+const username = "admin";
+const password = "password";
 const url = "http://127.0.0.1";
-const port = process.env.PORT || 3000;
+const port = 3000;
 const express = require("express");
 const app = express();
 var exec = require("child_process").exec;
